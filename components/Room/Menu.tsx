4e45d65eeac786f287a133402ea21a8bd1212fb1
Moomi98 <kymkjh2002@gmail.com>
2022-12-03 21:51:37 +0900

    비디오 메뉴 기본 레이아웃 구현

--- components/Room/Menu.tsx
+++ components/Room/Menu.tsx
@@ -1,14 +1,51 @@
 import styled from "styled-components";
+import IconTextButton from "../common/IconTextButton";
+import { AiFillSetting } from "react-icons/ai";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { videoMenu } from "../../constants/menus";
 
 const Container = styled.div`
   position: relative;
   width: 100%;
   height: 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
   background-color: black;
+  padding: 20px 30px;
+`;
+
+const LeaveRoomButton = styled.button`
+  padding: 10px 30px;
+  border: none;
+  border-radius: 5px;
+  background-color: red;
+  font-size: 16px;
+  font-weight: 500;
+  text-align: center;
+  color: white;
+  cursor: pointer;
+  transition: all 0.3s;
+  &:hover {
+    background-color: #d20000;
+  }
 `;
 
 const Menu = () => {
-  return <Container />;
+  return (
+    <Container>
+      <IconTextButton
+        icon={<AiFillSetting size={35} color="white" />}
+        content={videoMenu.SETTING}
+      />
+      <IconTextButton
+        icon={<FaChalkboardTeacher size={35} color="white" />}
+        content={videoMenu.WHITE_BOARD}
+      />
+      <LeaveRoomButton>{videoMenu.LEAVE_ROOM}</LeaveRoomButton>
+    </Container>
+  );
 };
 
 export default Menu;
---
+++ constants/menus.ts
@@ -0,0 +1,5 @@
+export const videoMenu = {
+  SETTING: "설정",
+  LEAVE_ROOM: "나가기",
+  WHITE_BOARD: "공유 보드",
+};
