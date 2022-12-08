84627ced183c3796a9afd08489f8adcb9023178a
Moomi98 <kymkjh2002@gmail.com>
2022-12-08 16:20:17 +0900

    스피커 설정 구현

--- components/setting/SpeakerSetting.tsx
+++ components/setting/SpeakerSetting.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { mediaStreamState } from "../../stores/stream";
+import { getDevices, getUserMedia } from "../../utils/channel/channel";
 
 const Container = styled.div`
   width: 100%;
@@ -21,14 +25,45 @@ const Title = styled.p`
   font-size: 20px;
 `;
 
-const SpeakerSetting = () => {
+const MicSelect = styled.select`
+  width: 100%;
+  height: 30px;
+`;
+const MicSetting = () => {
+  const [speakers, setSpeakers] = useState<MediaDeviceInfo[]>([]);
+  const [_, setMediaStream] = useRecoilState(mediaStreamState);
+
+  const setMicDevices = async () => {
+    const devices = await getDevices();
+    const speakerOptions = devices?.filter(
+      (device) => device.kind === "audiooutput"
+    );
+    setSpeakers([...speakerOptions]);
+  };
+
+  const changeSpeaker = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const speakerId = e.target.selectedOptions[0].value;
+    setMediaStream(await getUserMedia(speakerId));
+  };
+
+  useEffect(() => {
+    setMicDevices();
+  }, []);
+
   return (
     <Container>
       <SettingContainer>
         <Title>스피커 선택</Title>
+        <MicSelect onChange={changeSpeaker}>
+          {speakers?.map((speaker) => (
+            <option key={speaker.deviceId} value={speaker.deviceId}>
+              {speaker.label}
+            </option>
+          ))}
+        </MicSelect>
       </SettingContainer>
     </Container>
   );
 };
 
-export default SpeakerSetting;
+export default MicSetting;
