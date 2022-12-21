b9bb5d0ab19fbbb73866f82699f2b965255fd2b2
Moomi98 <kymkjh2002@gmail.com>
2022-12-21 23:08:43 +0900

    bug : 태그 공백 삽입 시 text overflow 현상 해결

--- components/common/Tag.tsx
+++ components/common/Tag.tsx
@@ -6,18 +6,21 @@ interface tagProps {
 }
 
 const Container = styled.div`
+  font-size: 16px;
+  font-weight: 500;
   background-color: #f2f2f2;
   color: ${colors.gray};
   border-radius: 5px;
   padding: 5px 10px;
-  max-width: 100px;
-  max-height: 30px;
+  max-width: 150px;
   overflow: hidden;
   text-overflow: ellipsis;
+  word-break: break-all;
+  white-space: nowrap;
 `;
 
 const Tag = ({ content }: tagProps) => {
-  return <Container>#{content}</Container>;
+  return <Container>#&nbsp;{content}</Container>;
 };
 
 export default Tag;
